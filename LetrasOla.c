13a3651b271f71f72f7cd1f3b2d067aad9c9ce04
lucascarvalho0405 <a31559@tomazpelayo.com>
2023-02-11 13:11:18 +0000

    f2 ex7

--- LetrasOla.c
+++ LetrasOla.c
@@ -2,9 +2,9 @@
 #include<stdio.h>
 int main()
 {
-    printf("  ###   #       ## \n");
-    printf(" #   #  #      #  #\n");
-    printf(" #   #  #     ###### \n");
-    printf("  ###   #### #      #\n");
+    printf("  ####   #        ## \n");
+    printf(" #    #  #       #  #\n");
+    printf(" #    #  #      ###### \n");
+    printf("  ####   ##### #      #\n");
     return(0);
 }
